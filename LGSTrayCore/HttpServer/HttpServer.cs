d80024eca99d4421018041b32b29b5e1537283b7
spreedated <21370839+spreedated@users.noreply.github.com>
2025-07-31 23:13:43 +0200

    strong named projects

--- LGSTrayCore/HttpServer/HttpServer.cs
+++ LGSTrayCore/HttpServer/HttpServer.cs
@@ -9,39 +9,6 @@ namespace LGSTrayCore.HttpServer
 {
     public class HttpServer : IHostedService, IDisposable
     {
-        #region IDisposable
-        private bool disposedValue;
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    _server.Dispose();
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-                // TODO: set large fields to null
-                disposedValue = true;
-            }
-        }
-
-        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        // ~WebServer()
-        // {
-        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        //     Dispose(disposing: false);
-        // }
-
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
-        #endregion
-
         private readonly AppSettings _appSettings;
         private readonly HttpControllerFactory _httpControllerFactory;
 
@@ -79,5 +46,29 @@ namespace LGSTrayCore.HttpServer
 
             return server;
         }
+
+        #region Dispose
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    this._server?.Dispose();
+                    this._serverCts?.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
